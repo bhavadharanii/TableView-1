d956a1da3b60bd7492df564827e8bb7cbb927b95
Fiuser <fiuser@miruthula-mac.local>
2022-10-26 11:50:39 +0530

    Initial Commit

---
+++ To-Do App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To-Do App
+//
+//  Created by Fiuser on 26/10/22.
+//  Copyright © 2022 Fiuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
